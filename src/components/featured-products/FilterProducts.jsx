54a395542b0656a69eaf6cf80bc00bc0afd7f7d4
pualine <prisypauline.com>
2024-02-22 05:45:56 -0800

    Created featured product heading and product catalog session

---
+++ src/components/featured-products/FilterProducts.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+
+
+const FilterProducts = () => {
+  return (
+    <div className="col-lg-8 text-end">
+      <ul className="nav nav-pills d-inline-flex text-center mb-5">
+        <li className="nav-item">
+          <a
+            className="d-flex m-2 py-2 bg-light rounded-pill active"
+            data-bs-toggle="pill"
+            href="#tab-1"
+          >
+            {" "}
+            <span className="text-dark" style={{ width: 130 }}>
+              {" "}
+              All Products
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="d-flex m-2 py-2 bg-light rounded-pill"
+            data-bs-toggle="pill"
+            href="#tab-2"
+          >
+            {" "}
+            <span className="text-dark" style={{ width: 130 }}>
+              {" "}
+              Vegetables
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="d-flex m-2 py-2 bg-light rounded-pill"
+            data-bs-toggle="pill"
+            href="#tab-3"
+          >
+            {" "}
+            <span className="text-dark" style={{ width: 130 }}>
+              {" "}
+              Fruits
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="d-flex m-2 py-2 bg-light rounded-pill"
+            data-bs-toggle="pill"
+            href="#tab-4"
+          >
+            {" "}
+            <span className="text-dark" style={{ width: 130 }}>
+              {" "}
+              Bread
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="d-flex m-2 py-2 bg-light rounded-pill"
+            data-bs-toggle="pill"
+            href="#tab-5"
+          >
+            {" "}
+            <span className="text-dark" style={{ width: 130 }}>
+              {" "}
+              Meat
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default FilterProducts;
