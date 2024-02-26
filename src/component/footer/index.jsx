88cee045e83fa65867e75513e8468980ebddf570
EaineDan <eaine.dan98@gmail.com>
2024-02-26 11:31:31 +0000

    footer

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -1,8 +1,17 @@
 
+import { AppBar, Toolbar, Typography} from "@mui/material";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Email
+          </Typography>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Icon with text
+          </Typography>       
+        </Toolbar>
+      </AppBar>
+  );
 }
-
