5453910a3d33c36e02c6ab72b1effa13aea649d1
EaineDan <eaine.dan98@gmail.com>
2024-02-26 06:32:19 +0000

    changes made

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,43 +1,38 @@
-import { AppBar, Toolbar, IconButton, Typography, Button, Box, InputLabel, FormControl, Select, MenuItem } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Button, TextField, Box, Grid } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
-
-
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="Age"
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" sx={{ my: '1rem' }} position="static">
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-evenly' }} >
+          <Button variant="text">Home</Button>
+          <Button variant="text">About Us</Button>
+          <Button variant="text">Our Products</Button>
+          <Button variant="text">Sell Now</Button>
+          <Button variant="text">Contact Us</Button>
+          <IconButton color="primary" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+          <Button color="warning">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
 
-        <AppBar color="transparent" sx={{my: '5rem'}} position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Agrobiz Logo
-                </Typography>
-                <Button variant="text">Home</Button>
-                <Button variant="text">About Us</Button>
-                <Button variant="text">Our Products</Button>
-                <Button variant="text">Sell Now</Button>
-                <Button variant="text">Contact Us</Button>
-                <IconButton color="primary" aria-label="add to shopping cart">
-          <AddShoppingCartIcon />
-        </IconButton>
-        <Button color="inherit">Sign Up</Button>
-      </Toolbar>
-    </AppBar>
+      <Grid sx={{ display: 'flex', justifyContent: 'center' }}>
+        <TextField
+          variant="outlined"
+          label="Search"
+          InputProps={{
+            endAdornment: (
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            ),
+          }}
+        />
+
+      </Grid>
     </Box>
   );
 }
