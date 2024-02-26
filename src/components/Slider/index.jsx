fa3f57722f48df40d5659505ef62852e2a15c35b
theophilusnuer <tphilus003@gmail.com>
2024-02-26 23:01:39 +0000

    updating the slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -3,36 +3,41 @@ import slider2 from'../../Assets/slider2.jpg'
 import slider3 from '../../Assets/slider3.jpg'
 import Carousel from 'react-bootstrap/Carousel';
 import styles from './index.module.css'
+import Button from 'react-bootstrap/Button';
 
 
 
 function Slider() {
   return (
     <Carousel pause='false'>
-      <Carousel.Item className={styles.imageContainer}  interval={2000}>
+      
+      <Carousel.Item className={styles.imageContainer}  interval={5000}>
         <img src= {slider1} className={styles.imageSizer} alt='' />
-        <Carousel.Caption>
-          <h3>Buy Now</h3>
-          <button>BUY NOW</button>
-          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+        <Carousel.Caption className={styles.captionBox}>
+        <h2 className={styles.headLine}>Quality Agriculture Products</h2>
+          <h4 className={styles.welcomeToAgro}>Welcome to AgroBiz. We believe in better Agriculture for better future</h4>
+          <button className={styles.callToAction}>Buy Here</button>
+          <button className={styles.callToAction}>Sell Here</button>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item className={styles.imageContainer}  interval={2000}>
+      <Carousel.Item className={styles.imageContainer}  interval={5000}>
       <img src= {slider2} className={styles.imageSizer} alt='' />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className={styles.captionBox}>
+        <h2 className={styles.headLine}>Quality Agriculture Products</h2>
+          <h4 className={styles.welcomeToAgro}>Welcome to AgroBiz. We believe in better Agriculture for better future</h4>
+          <button className={styles.callToAction}>Buy Here</button>
+          <button className={styles.callToAction}>Sell Here</button>
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item className={styles.imageContainer}  interval={2000}>
+      <Carousel.Item className={styles.imageContainer}  interval={5000}>
       <img src= {slider3} className={styles.imageSizer} alt='' />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <Carousel.Caption className={styles.captionBox}>
+        <h2 className={styles.headLine}>Quality Agriculture Products</h2>
+          <h4 className={styles.welcomeToAgro}>Welcome to AgroBiz. We believe in better Agriculture for better future</h4>
+          <Button variant='primary ' className={styles.callToAction}>Buy Here</Button>
+          <Button variant='primary ' className={styles.callToAction}>Sell Here</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
