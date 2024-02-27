f6b2031d5d0d48bda584d503b3f67c94728fd202
EaineDan <eaine.dan98@gmail.com>
2024-02-27 09:20:21 +0000

    updates

--- src/components/Seller Dashboard/index.jsx
+++ src/components/Seller Dashboard/index.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  Typography,
+  Container,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Grid,
+} from "@mui/material";
 
 const SellerDashboard = () => {
   return (
-   <></>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="transparent" sx={{ my: "1rem" }} position="static">
+        <Toolbar sx={{ display: "flex", justifyContent: "end" }}>
+          <Button variant="contained" color="primary">
+            My Profile
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Typography sx={{ mx: "1rem" }}>My store</Typography>
+      <Grid container spacing={3}>
+        <Grid xs={4}>
+            <Card
+              sx={{ maxWidth: 345, my: "2rem", mx: "3rem", height: "100%" }}
+            >
+              <CardMedia component="img" alt="item" height="160" image="" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Item name
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Provide product details here
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="medium">Edit</Button>
+                <Button size="medium">Delete</Button>
+              </CardActions>
+            </Card>
+        </Grid>
+      </Grid>
+
+      <Container sx={{ my: "2rem" }} maxWidth="sm">
+        <Button size="large" variant="outlined">
+          Start selling
+        </Button>
+      </Container>
+    </Box>
+  );
+};
 
 export default SellerDashboard;
