62d964d6846533064d0cbc34a53a47d04baf6389
theophilusnuer <tphilus003@gmail.com>
2024-02-28 18:34:47 +0000

    worked on testimonials

--- src/components/Testimonials/TestimonialsData.jsx
+++ src/components/Testimonials/TestimonialsData.jsx
@@ -1,24 +1,33 @@
-const testimonialsData = [
-    {
-      id: 1,
-      image: 'anisas',
-      name: 'Anisa',
-      title: 'Small-Scale Farmer',
-      description: '“As a small-scale farmer, I used to struggle with finding buyers for my fresh produce. But ever since I started using AgroBiz, my life has changed. The platform connects me directly with consumers who appreciate locally grown, organic fruits and vegetables. The seamless ordering process and timely payments have made my farming journey more sustainable and profitable.”'
-    },
-    {
-      id: 2,
-      image: 'abigails',
-      name: 'Abigail',
-      title: 'Urban Gardener',
-      description: '“Living in a city, I always dreamed of having my own garden. Thanks to AgroBiz, I can now order a variety of seeds, saplings, and gardening tools online. The website is user-friendly interface and detailed product descriptions make it easy for me to choose the right items. Plus, their blog section provides valuable tips on urban gardening. I have transformed my balcony into a lush green oasis!”'
-    },
-    {
-      id: 3,
-      image: 'ellas',
-      name: 'Ella',
-      title: 'Restaurant Owner',
-      description: '“Running a restaurant means sourcing high-quality ingredients consistently. FreshHarvest has become my go-to platform for procuring fresh produce directly from local farmers. The website’s real-time inventory updates, competitive pricing, and reliable delivery service have streamlined my supply chain. My customers appreciate the farm-to-table concept, and I credit it all to FreshHarvest.”'
-    },
-]
 
+
+
+import styles from './testimonialdata.module.css';
+// import Carousel from 'react-bootstrap/Carousel';
+
+
+
+
+
+
+const TestimonialData = (props) => {
+ 
+
+
+  return (
+  
+      <div className={styles.testimonial}>
+        <div className={styles.testimonialImage}>
+         <img src={props.src} alt="" />
+        </div>
+        <div className={styles.testimonialContent}>
+          <p>{props.description}</p>
+          <p className={styles.testimonialName}>{props.name}</p>
+          <p className={styles.testimonialName}>{props.title}</p>
+        </div>
+      </div>
+
+   
+  );
+};
+
+export default TestimonialData;
--- src/pages/Landing page/index.jsx
+++ src/pages/Landing page/index.jsx
@@ -3,13 +3,17 @@
 import Navbar from "../../components/Navbar";
 import Slider from "../../components/Slider";
 import Testimonials from "../../components/Testimonials";
+import TopSellers from "../../components/TopSellers";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <Slider />
+      <TopSellers />
       <Testimonials />
+      
+
     </div>
   )
 }
