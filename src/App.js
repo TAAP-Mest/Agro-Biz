88704f466f963fb1ea02b0003e0b72bca18e32b6
theophilusnuer <159792961+theophilusnuer@users.noreply.github.com>
2024-03-01 13:23:56 +0000

    Revert "add Seller dashboard to dev"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-
-
-import { Route, Routes } from "react-router-dom";
-import SellerDashboard from "./components/sellerDashboard";
-import AddNewProduct from "./components/Addanewproduct";
-import EditOldProduct from "./components/editOldProduct";
-
 import LandingPage from "./pages/LandingPage";
 import About from "./pages/AboutUs";
 import SellingPage from "./pages/SellingPage";
@@ -14,10 +7,9 @@ import Footer from "./components/footer";
 import MarketPlace from "./pages/MarketPlace";
 
 
-
 export default function App() {
   return (
-<>
+    <>
       <Navbar />
 
       <Routes>
@@ -25,12 +17,8 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/market" element={<MarketPlace />} />
         <Route path="/sell" element={<SellingPage />} />
-           <Route path="/sell" element={<SellerDashboard/>}/>
-      <Route path="/add" element={<AddNewProduct/>}/>
-      <Route path="/edit" element={<EditOldProduct/>}/>
       </Routes>
       <Footer />
-
     </>
   );
 }
--- src/components/Addanewproduct/productsitem/index.jsx
+++
@@ -1,5 +0,0 @@
-export default function ProductItem ({index,  item,description,file,quantity,price,contact}) {
-    return (
-        <span key={index}>{item} {description} {file} {quantity} {price} </span>
-    );
-}
